df277bbfa66865ff56df7483bd07a727dc8f128c
esteban2502 <juanes9001@gmail.com>
2025-08-12 22:08:10 -0500

    agrega anotacion de servicio

--- moca/src/main/java/com/ucp/moca/service/implement/TestServiceImpl.java
+++ moca/src/main/java/com/ucp/moca/service/implement/TestServiceImpl.java
@@ -4,9 +4,11 @@ import com.ucp.moca.entity.Test;
 import com.ucp.moca.repository.TestRepository;
 import com.ucp.moca.service.TestService;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TestServiceImpl implements TestService {
 
     private final TestRepository testRepository;
