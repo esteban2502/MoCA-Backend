77a70121ffd76d8e032d5552c8cb1cfcbce0a19b
esteban2502 <juanes9001@gmail.com>
2025-08-12 20:59:59 -0500

    agrega modularizacion de carpetas y variables de entorno

---
+++ moca/src/main/java/com/ucp/moca/MocaApplication.java
@@ -0,0 +1,13 @@
+package com.ucp.moca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MocaApplication.class, args);
+	}
+
+}
